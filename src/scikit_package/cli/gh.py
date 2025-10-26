d77a350b697dcf3335097a3693dc24b8f1f4046c
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-10-26 15:00:42 -0400

    chore: add the function signature and docstring

--- src/scikit_package/cli/gh.py
+++ src/scikit_package/cli/gh.py
@@ -1,13 +1,55 @@
-def _get_broadcast_urls():
-    """Get repo urls to broadcast from the interactions with users.
+def _get_broadcast_info_dict(url_file_path=None):
+    """Load the recognizable broadcast repository information from files
+    in the given path or GitHub repo.
 
-    This function interacts with the user in the following order:
-    1. Prompt for the location of files used to decipher the alias for
-    the target broadcast repo urls.
-    2. Print the recognizable name for each single repo and each
-    group of repos.
-    3. Prompt to let user type the name of target repos or groups of
-    repos to broadcast the issue.
+    If ``url_file_path`` is not None, first try to treat it as an URL, and if
+    that fails, treat it as a directory path. If both fail, raise
+    ``ValueError``. If ``url_file_path`` is None, the broadcast_url_file_path
+    set in ``~/.skpkgrc`` will be used. If it is not found, use the current
+    working directory as the ``url_file_path``.
+
+    If the file ``groups.json`` and ``repos.json`` don't exist in
+    ``url_file_path``, raise ``ValueError``.
+
+    Parameters
+    ----------
+    url_file_path : str
+        The file path or URL recognizable broadcast repository information.
+        If it is None, the ``broadcast_url_file_path`` set in
+        ``~/.skpkgrc`` will be used. If it is not found, use the current
+        working directory as the ``url_file_path``.
+
+    Returns
+    -------
+    broadcast_info_dict : dict
+        The dict containing recognizable broadcast repo URLs.
+    """
+    broadcast_repos_dict = {}
+    return broadcast_repos_dict
+
+
+def _print_recognized_broadcast_repos(broadcast_repos_dict):
+    """Print the recognized broadcast repository URLs from the dict.
+
+    Parameters
+    ----------
+    broadcast_repos_dict : dict
+        The dict containing recognizable broadcast repo URLs.
+    """
+    return None
+
+
+def _get_broadcast_urls(selected_names, broadcast_repos_dict):
+    """Get the urls and package names of all repositories to broadcast
+    to.
+
+    Parameters
+    ----------
+    selected_names : str
+        The input string for selected_names. It takes the form:
+        "repo1,repo2,group1"
+    broadcast_repos_dict : dict
+        The dict containing recognizable broadcast repo URLs.
 
     Returns
     -------
